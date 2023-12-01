c045662ddd3df1d3ac5c8ce05df9d4932385bc3c
Sergei Popinevskii <gurza000@gmail.com>
2023-12-02 02:17:16 +0300

    add test cases from part two

--- cmd/day01/main_test.go
+++ cmd/day01/main_test.go
@@ -7,11 +7,17 @@ func TestGetCalibrationValue(t *testing.T) {
 		in   string
 		want int
 	}{
-		{"123456", 16},
 		{"1abc2", 12},
 		{"pqr3stu8vwx", 38},
 		{"a1b2c3d4e5f", 15},
 		{"treb7uchet", 77},
+		{"two1nine", 29},
+		{"eightwothree", 83},
+		{"abcone2threexyz", 13},
+		{"xtwone3four", 24},
+		{"4nineeightseven2", 42},
+		{"zoneight234", 14},
+		{"7pqrstsixteen", 76},
 	}
 
 	for _, tc := range testCases {
