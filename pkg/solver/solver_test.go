8622a06fc5174e3f2d2520c9a51065caf320ee8b
Sergei Popinevskii <gurza000@gmail.com>
2023-12-07 17:24:01 +0300

    add test case for SumAdjacentLines func

--- pkg/solver/solver_test.go
+++ pkg/solver/solver_test.go
@@ -19,16 +19,37 @@ func sumInts(batch []string) (int, error) {
 }
 
 func TestSumAdjacentLines(t *testing.T) {
-	h := sumInts
-	rdr := strings.NewReader("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n")
-	// For this test case, each line is added 3 times except the first
-	// and last lines, which are added twice
-	want := 2*(0+9) + 3*(1+2+3+4+5+6+7+8)
+	testCases := []struct {
+		h    BatchHandler
+		n    int
+		in   string
+		want int
+	}{
+		{
+			h:  sumInts,
+			n:  1,
+			in: "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n",
+			// For this test case, each line is added 3 times except the first
+			// and last lines, which are added twice
+			want: 2*(0+9) + 3*(1+2+3+4+5+6+7+8), // 126
+		},
+		{
+			h:    sumInts,
+			n:    2,
+			in:   "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n",
+			want: 198,
+		},
+	}
 
-	got, err := SumAdjacentLines(h, 1, rdr)
-	if err != nil {
-		t.Errorf("SumAdjacentLines returned an error: %v", err)
-	} else if got != want {
-		t.Errorf("SumAdjacentLines = %d; want %d", got, want)
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			rdr := strings.NewReader(tc.in)
+			got, err := SumAdjacentLines(tc.h, tc.n, rdr)
+			if err != nil {
+				t.Errorf("SumAdjacentLines returned an error: %v", err)
+			} else if got != tc.want {
+				t.Errorf("SumAdjacentLines = %d; want %d", got, tc.want)
+			}
+		})
 	}
 }
